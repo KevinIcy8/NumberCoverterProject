39bd64057decde7c4bc0eaa3fff615de1ef36ff1
KevinIcy8 <kouyang7573@bths.edu>
2023-03-09 01:12:46 -0500

    fixed bug; finished two challenges

--- src/ConverterRunner.java
+++ src/ConverterRunner.java
@@ -3,44 +3,91 @@ import java.util.Arrays;
 
 class ConverterRunner {
     public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
         System.out.println("Welcome to the Number Converter!");
         System.out.println("--------------------------------");
-        System.out.print("Enter the base of your number (2, 8 or 10): ");
+        System.out.print("Enter \"A\" for normal base conversion or Enter \"B\" for decimal --> base 1-64 conversion: ");
+        String option = s.nextLine();
+        option = option.toUpperCase();
+        if(option.equals("B")){
+            System.out.print("Enter a decimal number: ");
+            String num = s.nextLine();
+            System.out.print("What base would you like to convert it to(1-64): ");
+            String b = s.nextLine();
+            NumberConverter ac = new NumberConverter(Integer.parseInt(num),Integer.parseInt(b));
+            String converted = ac.displayNormalNumber2(ac.convertToAny());
+            System.out.print("Decimal --> Base " + b + ": " + converted);
+        }
+        else{
+        System.out.print("Enter the base of your number (2, 8, 10, or 16(Hexadecimal)): ");
 
-        Scanner s = new Scanner(System.in);
         String choice = s.nextLine();
         while(!NumberConverter.validBase(choice)){
             System.out.print("You entered an invalid base number! Please enter an integer for you base: ");
             choice = s.nextLine();
-            NumberConverter.validBase(choice);
         }
         int base = Integer.parseInt(choice);
 
         System.out.print("Enter your number: ");
         String number = s.nextLine();
-        while(!(NumberConverter.validNumber(number, choice))){
-            System.out.println("You entered an invalid number");
+        if(base != 16) {
+            while (!(NumberConverter.validNumber(number, base))) {
+                System.out.print("You entered an invalid number! Please enter a valid number corresponding to the base: ");
+                number = s.nextLine();
+            }
         }
-        int n = Integer.parseInt(number);
+        else{
+            while (!(NumberConverter.validNumberHex(number))) {
+                System.out.print("You entered an invalid number! Please enter a valid number corresponding to the base: ");
+                number = s.nextLine();
+            }
+        }
+
+        int n = 0;
+        String l = "";
+        if(base != 16){
+            n = Integer.parseInt(number);
+        }
+        else{
+            l = number;
+        }
 
         s.close();
-
-        NumberConverter nc = new NumberConverter(n, base);
+        NumberConverter nc = new NumberConverter(10, 8);
+        NumberConverter lc = new NumberConverter("2A", 10);
+        if(base!= 16) {
+            nc = new NumberConverter(n, base);
+        }
+        else{
+            lc = new NumberConverter(l, base);
+        }
         if(base == 10){
-            System.out.println("\nBinary Number: " + nc.displayNormalNumber(nc.convertToBinary(nc.displayNormalNumber(nc.getDigits()))));
-            System.out.println("Octal Number: " + nc.displayNormalNumber(nc.convertToOctal(nc.getDigits())));
+            System.out.println("\nBinary Number: " + nc.displayNormalNumber(nc.convertToBinary(String.valueOf(n))));
+            System.out.println("Octal Number: " + nc.displayNormalNumber(nc.convertToOctal(String.valueOf(n))));
+            System.out.println("Hexadecimal Number: " + nc.displayNormalNumber2(nc.convertToHex(String.valueOf(n))));
         }
         else if(base == 2){
             System.out.println("\nDecimal Number: " + nc.displayNormalNumber(nc.convertToDecimal()));
-            System.out.println("Octal Number: " + nc.displayNormalNumber(nc.convertToOctal(nc.convertToDecimal())));
+            String decimal = nc.displayNormalNumber(nc.convertToDecimal());
+            System.out.println("Octal Number: " + nc.displayNormalNumber(nc.convertToOctal(decimal)));
+            System.out.println("Hexadecimal Number: " + nc.displayNormalNumber2(nc.convertToHex(decimal)));
         }
         else if(base == 8){
-            System.out.println("\nBinary Number: " + nc.displayNormalNumber(nc.convertToBinary(nc.displayNormalNumber(nc.convertToDecimal()))));
+            String decimal = nc.displayNormalNumber(nc.convertToDecimal());
+            System.out.println("\nBinary Number: " + nc.displayNormalNumber(nc.convertToBinary(decimal)));
             System.out.println("Decimal Number: " + nc.displayNormalNumber(nc.convertToDecimal()));
+            System.out.println("Hexadecimal Number: " + nc.displayNormalNumber2(nc.convertToHex(decimal)));
+        }
+        else if(base == 16){
+            String decimal = lc.convertToDecimalGreater10();
+            System.out.println("\nDecimal Number: " + decimal);
+            System.out.println("Binary Number: " + nc.displayNormalNumber(nc.convertToBinary(decimal)));
+            System.out.println("Octal Number: " + nc.displayNormalNumber(nc.convertToOctal(decimal)));
         }
         int[] digits = nc.getDigits();
         System.out.println("\n\nDigit array: " + Arrays.toString(digits));
         System.out.println("Number: " + nc.displayOriginalNumber());
+        }
     }
 }
 
--- src/NumberConverter.java
+++ src/NumberConverter.java
@@ -1,7 +1,9 @@
 
 public class NumberConverter {
     int[] digits;
+    String[] digits2;
     int number;
+    String fullNumber;
     int base;
 
     public NumberConverter(int number, int base) {
@@ -16,6 +18,18 @@ public class NumberConverter {
         this.base = base;
     }
 
+    public NumberConverter(String number, int base){
+        String numberAsString = number;
+        digits2 = new String[numberAsString.length()];
+        for (int i = 0; i < numberAsString.length(); i++) {
+            String single = numberAsString.substring(i,i+1);
+            String d = single;
+            digits2[i] = d;
+        }
+        this.fullNumber = number;
+        this.base = base;
+    }
+
     public String displayOriginalNumber() {
         String o = "";
         for (int i = 0; i < digits.length; i++) {
@@ -26,6 +40,14 @@ public class NumberConverter {
     }
 
     public static boolean validBase(String choice){
+        if(choice.equals("2") || choice.equals("8") || choice.equals("10") || choice.equals("16")){
+            return true;
+        }
+        return false;
+    }
+
+
+    public static boolean validBase2(String choice){
         String numbers = "0123456789";
         for(int i = 0; i < choice.length(); i++){
             String eachNumber = choice.substring(i,i+1);
@@ -44,18 +66,26 @@ public class NumberConverter {
         return true;
     }
 
-    public static boolean validNumber(String number, String base){
+    public static boolean validNumber(String number, int base){
         for(int i = 0; i < number.length(); i++){
             String eachNumber = number.substring(i,i+1);
             int count = 0;
-            if(Integer.parseInt(eachNumber) >= Integer.parseInt(base)){
+            if(Integer.parseInt(eachNumber) >= base){
                 count++;
             }
             if(count > 0){
                 return false;
             }
-            else{
-                return true;
+        }
+        return true;
+    }
+
+    public static boolean validNumberHex(String number){
+        String hexVariable = "0123456789ABCDEFabcdef";
+        for(int i = 0; i < number.length(); i++){
+            String eachNumber = number.substring(i,i+1);
+            if(!hexVariable.contains(eachNumber)){
+                return false;
             }
         }
         return true;
@@ -70,11 +100,28 @@ public class NumberConverter {
         for (int i = 0; i < conversion.length; i++) {
             o = o + conversion[i];
         }
-        o = o + "\n";
+        return o;
+    }
+
+    public String displayNormalNumber2(String[] conversion){
+        String o = "";
+        for (int i = 0; i < conversion.length; i++) {
+            o = o + conversion[i];
+        }
         return o;
     }
 
     public int[] convertToDecimal() {
+        /*if(base > 10){
+            int initialPower = digits2.length - 1;
+            int sum = 0;
+            for(int i = 0; i < digits2.length; i++){
+                sum = sum + (digits2[i] * (int)Math.pow(base, initialPower));
+                initialPower--;
+            }
+        }
+
+         */
         int initialPower = digits.length - 1;
         int sum = 0;
         for(int i = 0; i < digits.length; i++){
@@ -87,6 +134,43 @@ public class NumberConverter {
             decimal[i] = Integer.parseInt(sumString.substring(i,i+1));
         }
         return decimal;
+    }
+
+    public String convertToDecimalGreater10(){
+        String letters = "ABCDEF";
+        String[] digit = new String[digits2.length];
+        for(int i = 0; i < digits2.length; i++){
+            if(letters.contains(digits2[i])){
+                if(digits2[i].equals("A")){
+                    digit[i] = "10";
+                }
+                if(digits2[i].equals("B")){
+                    digit[i] = "11";
+                }
+                if(digits2[i].equals("C")){
+                    digit[i] = "12";
+                }
+                if(digits2[i].equals("D")){
+                    digit[i] = "13";
+                }
+                if(digits2[i].equals("E")){
+                    digit[i] = "14";
+                }
+                if(digits2[i].equals("F")){
+                    digit[i] = "15";
+                }
+            }
+            else {
+                digit[i] = digits2[i];
+            }
+        }
+        int initialPower = digit.length - 1;
+        int sum = 0;
+        for(int i = 0; i < digit.length; i++){
+            sum = sum + (Integer.parseInt(digit[i]) * (int)Math.pow(base, initialPower));
+            initialPower--;
+        }
+        return String.valueOf(sum);
     }
 
     public int[] convertToBinary(String decimal) {
@@ -109,27 +193,84 @@ public class NumberConverter {
         return binary;
     }
 
-    public int[] convertToOctal(int[] decimal) {
-        int quotient = number;
-        int octal[] = new int[quotient];
+    public int[] convertToOctal(String decimal) {
+        int quotient = Integer.parseInt(decimal);
+        int[] octal;
         int count = 0;
-        if(base == 10){
-            while(quotient != 0){
-                quotient = quotient/8;
-                count++;
-            }
-            octal = new int[count];
-            quotient = number;
-            for(int i = octal.length; i > 0; i--){
-                int input = quotient%8;
-                quotient = quotient/8;
-                octal[i-1] = input;
-            }
+        while(quotient != 0){
+            quotient = quotient/8;
+            count++;
         }
-        else if(base == 2){
+        octal = new int[count];
+        quotient = Integer.parseInt(decimal);
+        for(int i = octal.length-1; i >= 0; i--){
+            int input = quotient%8;
+            quotient = quotient/8;
+            octal[i] = input;
+        }
 
-        }
         return octal;
     }
+
+    public String[] convertToHex(String decimal){
+        int quotient = Integer.parseInt(decimal);
+        String[] hex;
+        int count = 0;
+        while(quotient != 0){
+            quotient = quotient/16;
+            count++;
+        }
+        hex = new String[count];
+        quotient = Integer.parseInt(decimal);
+        for(int i = hex.length -1; i >= 0; i--){
+            String input = String.valueOf(quotient % 16);
+            quotient = quotient/16;
+            if(Integer.parseInt(input) >= 10){
+                if(input.equals("10")){
+                    input = "A";
+                }
+                if(input.equals("11")){
+                    input = "B";
+                }
+                if(input.equals("12")){
+                    input = "C";
+                }
+                if(input.equals("13")){
+                    input = "D";
+                }
+                if(input.equals("14")){
+                    input = "E";
+                }
+                if(input.equals("15")){
+                    input = "F";
+                }
+            }
+            hex[i] = input;
+        }
+        return hex;
+    }
+
+    public String[] convertToAny(){
+        String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz+/";
+        int quotient = number;
+        String[] any;
+        int count = 0;
+        while(quotient != 0){
+            quotient = quotient/base;
+            count++;
+        }
+        any = new String[count];
+        quotient = number;
+        for(int i = any.length -1; i >= 0; i--) {
+            String input = String.valueOf(quotient % base);
+            quotient = quotient / base;
+            if(Integer.parseInt(input) >= 10){
+                input = letters.substring(Integer.parseInt(input)-10,Integer.parseInt(input)-10+1);
+            }
+            any[i] = input;
+        }
+        return any;
+    }
+
 }
 
